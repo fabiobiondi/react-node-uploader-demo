ad2fa123d453dbaf7e4bed1ac3b8e08a7ed5a203
Luciano <lucianomammino@gmail.com>
2021-02-15 18:17:11 +0000

    Improved dummy http example

--- stream-examples/06-http-dummy.js
+++ stream-examples/06-http-dummy.js
@@ -10,6 +10,9 @@ const server = createServer((req, res) => {
   // req is the stream representing the body of the incoming request
   // res is the stream that allows us to write a response body back to the client
 
+  // let's print all the headers received
+  console.log('\n\n', req.headers)
+
   // let's print on the standard output all the data that is coming from the request
   req.pipe(process.stdout)
   // when all the data has been printed let's reply to the client and close the response stream
@@ -24,3 +27,19 @@ server.listen(8000, () => console.log('Server started at http://localhost:8000')
 // now start the server with `node 06-http-dummy.js` and, in another terminal, try sending it some files with `curl`, for example:
 //
 // curl -F 'red-dot=@sample-files/red.gif' http://localhost:8000
+//
+// curl -F 'blue-dot=@sample-files/blue.gif' http://localhost:8000
+//
+// curl -F 'green-dot=@sample-files/green.gif' http://localhost:8000
+//
+// curl -F 'avatar=@sample-files/profile.gif' http://localhost:8000
+//
+//
+// You can even send multiple files and fields at the same time:
+//
+//
+// curl -F name=luciano -F avatar=@sample-files/profile.gif -F favourite_color=@sample-files/blue.gif http://localhost:8000
+//
+// If you look at the logs of the web server now, you can learn a lot about how the multipart/form-data protocol works.
+//
+// If you are curius to find out more, you can always take a look at the official spec: https://tools.ietf.org/html/rfc7578
